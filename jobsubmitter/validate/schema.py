9bbbcb1a778f80ce78e07359e0d97e9e404316f2
Benjamin Wingfield <bwingfield@ebi.ac.uk>
2023-06-08 15:13:59 +0100

    bundle JSON schema with job submitter

--- jobsubmitter/validate/schema.py
+++ jobsubmitter/validate/schema.py
@@ -10,9 +10,11 @@ def create_validator() -> Draft202012Validator:
      Don't use jsonschema.validate to validate multiple instances. The Draft202012Validator will cache the resolved
      references after the first fetch from pgsc_calc repository (checked against debug logs) .
     """
-    schema = json.loads(importlib.resources.read_text(schemas, 'api.json'))
+    schemas_path = importlib.resources.path(schemas, 'api.json').parent
 
-    resolver = RefResolver(str(importlib.resources.path(schemas, "api.json")), schema)
+    schema = json.loads(importlib.resources.read_text(schemas, 'api.json'))
+    resolver = RefResolver(base_uri=f"{schemas_path.as_uri()}/",
+                           referrer=schema)
 
     return Draft202012Validator(schema, resolver)
 
